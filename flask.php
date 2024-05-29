7301052b7e8e9e1c07121afe8264cbdb77bed1c3
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-30 01:07:49 +0800

    ggwp

--- flask.php
+++ flask.php
@@ -12,13 +12,16 @@
 
 <body class="flex flex-col bg-black text-white text-center w-full p-4 mx-auto text-wrap">
     <div>
-        <h1><strong class="text-3xl bg-gradient-to-r from-emerald-500 via-teal-500 to-sky-500 text-transparent bg-clip-text">So...
+        <h1><strong
+                class="text-3xl bg-gradient-to-r from-emerald-500 via-teal-500 to-sky-500 text-transparent bg-clip-text">So...
                 What is Flask exactly?</strong></h1>
         <p class="mt-3 mb-3">Flask is a Python library meant for web development. It can use HTML templates to return to
             the client, but
             it remains as a server side first framework which also allows you to write API's in it.</p>
-        <p>It uses the templates folder to return the HTML files that you've written with your custom
+        <p class="mb-3">It uses the templates folder to return the HTML files that you've written with your custom
             styling and templating, which can also use Python code to make the HTML temtlate dynamic.</p>
+        <p>This makes writing API's and server code easier as it allows the developer to get things out quicker using
+            Python due to it's easy syntax.</p>
     </div>
 </body>
 
