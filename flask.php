2c4dfd90c87302927c228f4199417ec38a5f234a
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-30 00:59:59 +0800

    first commit

---
+++ flask.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="index.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+    <title>Flask Tutorial</title>
+</head>
+
+<body class="flex flex-col bg-black text-white text-center w-full p-4 mx-auto text-wrap">
+    <div>
+        <h1><strong class="text-3xl bg-gradient-to-r from-emerald-500 via-teal-500 to-sky-500 text-transparent bg-clip-text">So...
+                What is Flask exactly?</strong></h1>
+        <p class="mt-3 mb-3">Flask is a Python library meant for web development. It can use HTML templates to return to
+            the client, but
+            it remains as a server side first framework which also allows you to write API's in it.</p>
+        <p>It uses the templates folder to return the HTML files that you've written with your custom
+            styling and templating, which can also use Python code to make the HTML temtlate dynamic.</p>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flask</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+    <link rel="stylesheet" href="index.css" />
+</head>
+
+<body class="bg-black text-white flex flex-col text-center w-full p-4 mx-auto text-wrap font-light">
+    <div>
+        <h1><strong class="text-7xl bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 bg-clip-text text-transparent">
+                <?php echo "Hey there!"; ?>
+            </strong>
+        </h1>
+        <hr class="rounded-lg h-[3px] bg-gradient-to-r from-emerald-500 via-fuchsia-500 to-yellow-500 mb-2 mt-3" />
+        <p class="text-2xl">This is a very basic tutorial of how to use Flask.</p>
+        <a href="flask.php" class="button bg-gradient-to-r from-sky-300 via-gray-300 to-yellow-300 bg-clip-text text-transparent">Click
+            here</a>
+    </div>
+</body>
+
+</html>
